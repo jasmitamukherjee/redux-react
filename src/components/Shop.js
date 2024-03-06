e7c3b5d4626dd75d10512f69612ad6bfa4287215
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-06 17:25:35 +0530

    initial

---
+++ src/components/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Shop() {
+  return (
+    <div>
+        <h2>
+           Deposit/Withdraw money
+        </h2>
+        <button className="btn btn-primary mx-2">
+           - 
+        </button>
+       Update Balance
+        <button className="btn btn-primary mx-2">
+           +
+        </button>
+        
+    </div>
+  )
+}
